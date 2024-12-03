84029632d572e4bd58c663b3c53b5a90ebd56562
Roberto Bagnara <ge65naw@mytum.de>
2024-12-03 15:35:35 +0100

    R scripts

--- opendc-simulator/opendc-simulator-compute/src/main/java/org/opendc/simulator/compute/power/battery/greenenergy/SimpleCarbonPolicy.java
+++ opendc-simulator/opendc-simulator-compute/src/main/java/org/opendc/simulator/compute/power/battery/greenenergy/SimpleCarbonPolicy.java
@@ -1,7 +1,7 @@
 package org.opendc.simulator.compute.power.battery.greenenergy;
 
 public class SimpleCarbonPolicy implements CarbonPolicy {
-    private final double carbonIntensityThreshold = 100;
+    private final double carbonIntensityThreshold = 200;
 
     @Override
     public boolean greenEnergyAvailable(double carbonIntensity, long now) {
